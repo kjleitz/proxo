59187e9e6e0ccc618af28537dbe20822f953a36b
Keegan Leitz <kjleitz@gmail.com>
2021-07-29 10:50:39 -0400

    initial commit

---
+++ lib/proxo.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "proxo/version"
+
+module Proxo
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/proxo_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Proxo do
+  it "has a version number" do
+    expect(Proxo::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
